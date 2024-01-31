e076347524a1659df6c2e0c219ed5bb12476da02
SosaWebDev <sjulioemanuel@gmail.com>
2024-01-30 19:36:40 -0600

    cards information added

--- site.js
+++ site.js
@@ -1,22 +1,79 @@
 
+const getCardInfo = index => {
 
-const cardInfo1 = {
-    "title": "Nike",
-	"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "image": "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "color": "#deaf01"
+
+    if(index === 0) return {
+        "title": "Nike",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "image": "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "color": "#deaf01"
+    }
+
+    if(index === 1) return {
+        "title": "Independent",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "image": "https://images.pexels.com/photos/5657417/pexels-photo-5657417.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "color": "#554a33"
+    }
+
+    if(index === 2) return {
+        "title": "Michael Kors",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "image": "https://images.pexels.com/photos/2783873/pexels-photo-2783873.jpeg",
+        "color": "#01322f"
+    }
+
+
+const title = "Champion"
+const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+const image = "https://images.pexels.com/photos/2783873/pexels-photo-2783873.jpeg"
+const color = "#ff0000"
+
+if (index === 3) return {title, text, image, color}
+
+}
+//console.log({cardInfo4})
+
+// function bla(text) {
+// console.log(text || "jejeje")
+// return 5;
+// }
+
+// console.log(bla())
+
+const createCard = (card, {image, title, text, color}) =>{
+
+    card.style.backgroundImage = `url(${image})`
+    card.style.color = color
+
+    const h1 = card.querySelector('h1')
+    h1.textContent = title
+
+    const p = card.querySelector('p')
+    p.textContent = text
 }
 
-const cardInfo2 = {
-    "title": "Independent",
-	"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "image": "https://images.pexels.com/photos/5657417/pexels-photo-5657417.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "color": "#554a33"
+const cards = document.querySelectorAll('.card')
+for (let i = 0; i < cards.length; i++){
+    createCard(cards[i], getCardInfo(i))
 }
 
-const cardInfo3 = {
-    "title": "Michael Kors",
-	"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "image": "https://images.pexels.com/photos/2783873/pexels-photo-2783873.jpeg",
-    "color": "#01322f"
-}
+const greetings = ({name}) => `Hello ${name}!`
+
+console.log(greetings({"name": "Julio"}))
+
+
+
+// setTimeout(() => createCard (cards[0], cardInfo1), 3000)
+
+const names = ["Ryan", "Marek", "Ashton", "Aidan"]
+
+console.log(names[names.length -1])
+
+const [ least, fav] = names
+console.log({least, fav})
+
+const sentence =  'This are words'
+const [first] = sentence.split('')
+
+console.log({first})
