fe64124fc432602143e5a50dfc251b4ff42dd91c
SosaWebDev <sjulioemanuel@gmail.com>
2024-02-06 19:52:28 -0600

    added infinite horizontal scroll and 1 extra card added using javascript

--- site.js
+++ site.js
@@ -1,49 +1,64 @@
 
-const getCardInfo = index => {
-
-
-    if(index === 0) return {
+const products = [{
         "title": "Nike",
         "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        "image": "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "image": "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg",
         "color": "#deaf01"
-    }
-
-    if(index === 1) return {
+    },{
         "title": "Independent",
         "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        "image": "https://images.pexels.com/photos/5657417/pexels-photo-5657417.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "image": "https://images.pexels.com/photos/5657417/pexels-photo-5657417.jpeg",
         "color": "#554a33"
-    }
-
-    if(index === 2) return {
+    },{
         "title": "Michael Kors",
         "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         "image": "https://images.pexels.com/photos/2783873/pexels-photo-2783873.jpeg",
         "color": "#01322f"
+    },{
+        "title": "Champion",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "image": "https://images.pexels.com/photos/10435675/pexels-photo-10435675.jpeg",
+        "color": "#554a33"
+    },{
+        "title": "Iphone",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "image": "https://images.pexels.com/photos/1092644/pexels-photo-1092644.jpeg",
+        "color": "deaf01"
+    },{
+        "title": "Car",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "image": "https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg",
+        "color": "#01322f"
     }
+]
 
 
-const title = "Champion"
-const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-const image = "https://images.pexels.com/photos/2783873/pexels-photo-2783873.jpeg"
-const color = "#ff0000"
+const container = document.querySelector('#container')
+const previousButton = document.querySelector('button.previous')
+const nextButton = document.querySelector('button.next')
 
-if (index === 3) return {title, text, image, color}
+container.removeChild(nextButton.parentNode )
 
-}
-//console.log({cardInfo4})
+const newCard = document.createElement('div')
+newCard.id = "card-4"
+newCard.classList.add('card')
+container.append(newCard)
 
-// function bla(text) {
-// console.log(text || "jejeje")
-// return 5;
-// }
+const infoDiv = document.createElement('div')
+newCard.append(infoDiv)
 
-// console.log(bla())
+const header  = document.createElement('h1')
+infoDiv.append(header)
 
-const createCard = (card, {image, title, text, color}) =>{
+const paragraph  = document.createElement('p')
+infoDiv.append(paragraph)
 
-    card.style.backgroundImage = `url(${image})`
+container.append(nextButton.parentNode)
+
+
+const displayProduct = (card, { image, title, text, color }) => {
+
+    card.style.backgroundImage = `url(${image}?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`
     card.style.color = color
 
     const h1 = card.querySelector('h1')
@@ -54,26 +69,66 @@ const createCard = (card, {image, title, text, color}) =>{
 }
 
 const cards = document.querySelectorAll('.card')
-for (let i = 0; i < cards.length; i++){
-    createCard(cards[i], getCardInfo(i))
+
+
+let offset = 0
+
+const updateCards = shift => {
+
+    offset += shift || 0
+    cards.forEach((card, index) => { 
+    const productIndex = (index + offset) % products.length
+    displayProduct(card, products[productIndex])
+    })
+
 }
 
-const greetings = ({name}) => `Hello ${name}!`
-
-console.log(greetings({"name": "Julio"}))
+updateCards(0)
 
 
 
-// setTimeout(() => createCard (cards[0], cardInfo1), 3000)
+previousButton.addEventListener('click',() => updateCards(-1))
+nextButton.addEventListener('click',() => updateCards(1))
 
-const names = ["Ryan", "Marek", "Ashton", "Aidan"]
 
-console.log(names[names.length -1])
 
-const [ least, fav] = names
-console.log({least, fav})
+// const greetings = ({name}) => `Hello ${name}!`
 
-const sentence =  'This are words'
-const [first] = sentence.split('')
+// //console.log(greetings({"name": "Julio"}))
 
-console.log({first})
+
+
+// // setTimeout(() => createCard (cards[0], cardInfo1), 3000)
+
+// const names = ["ryan", "marek", "ashton", "aidan"]
+// const names2 = ["tevin", ...names.slice(1,4) ]
+
+// const uppercase = text => 
+//     `${text.at(0).toUpperCase()}${text.slice(1).toLowerCase()}`
+
+// names2 .forEach(name => console.log(uppercase(name)))
+
+// const input = "Valentines Day"
+
+// if (['Christmas', 'Halloween', 'St. Patricks Day', 'Valentines Day'].includes(input)){
+//     console.log("FOUND")
+// }
+
+//console.log(names2)
+
+// console.log(names2[names2.length -1])
+
+// const [ least, fav] = names2
+// console.log({least, fav})
+
+// const sentence =  'This are words'
+// const [first] = sentence.split('')
+
+// console.log({first})
+
+// const printNames = (name1, name2) => {
+//     console.log(name1)
+//     console.log(name2)
+// }
+
+// printNames (...names)
